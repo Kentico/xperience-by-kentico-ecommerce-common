dfe26245725ce1746bdca7e58a1ccb950eaf453c
Martin Kyjac <martin.kyjac@bluesoft.cz>
2025-02-11 17:22:05 +0100

    Add common workspaces functionality

---
+++ src/Kentico.Xperience.Ecommerce.Common/Admin/EcommerceCommonConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Kentico.Xperience.Ecommerce.Common.Admin
+{
+    public static class EcommerceCommonConstants
+    {
+        public const string SettingsProductSKUFolderGuid = "Content item folder for PRODUCT content items";
+        public const string SettingsProductVariantFolderGuid = "Content item folder for PRODUCT VARIANT content items";
+        public const string SettingsProductImageFolderGuid = "Content item folder for IMAGES";
+        public const string SettingsWorkspaceName = "Content item workspace name";
+    }
+}
---
+++ src/Kentico.Xperience.Ecommerce.Common/Admin/Providers/WorkspaceOptionsProvider.cs
@@ -0,0 +1,29 @@
+﻿using CMS.DataEngine;
+using CMS.Helpers;
+using CMS.Workspaces;
+using Kentico.Xperience.Admin.Base.FormAnnotations;
+
+namespace Kentico.Xperience.Ecommerce.Common.Admin.Providers
+{
+    public class WorkspaceOptionsProvider(
+        IInfoProvider<WorkspaceInfo> workspaceInfoProvider,
+        IProgressiveCache cache) : IDropDownOptionsProvider
+    {
+        public async Task<IEnumerable<DropDownOptionItem>> GetOptionItems() =>
+            (await cache.LoadAsync(
+                    GetWorkspaces,
+                    new CacheSettings(120, $"{nameof(WorkspaceOptionsProvider)}|{nameof(GetOptionItems)}")))
+                .Select(w => new DropDownOptionItem()
+                {
+                    Value = w.WorkspaceName,
+                    Text = w.WorkspaceDisplayName
+                });
+
+        private async Task<IEnumerable<WorkspaceInfo>> GetWorkspaces(CacheSettings cs)
+        {
+            cs.CacheDependency = CacheHelper.GetCacheDependency($"{WorkspaceInfo.OBJECT_TYPE}|all");
+
+            return await workspaceInfoProvider.Get().GetEnumerableTypedResultAsync();
+        }
+    }
+}
