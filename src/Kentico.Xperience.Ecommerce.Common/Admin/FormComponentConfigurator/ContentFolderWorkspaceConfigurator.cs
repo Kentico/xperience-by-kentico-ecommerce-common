d55fb1fbe09c6d1fe168a09872f8acca85ab846b
Martin Kyjac <159790459+martinkyjac@users.noreply.github.com>
2025-04-01 10:37:13 +0200

    Add common workspaces functionality (#12)
    * Add common workspaces functionality
    
    * Whitespace formatting fix
    
    * Use string.Equals instead of ==
    
    * Add new lines to separate new class

---
+++ src/Kentico.Xperience.Ecommerce.Common/Admin/FormComponentConfigurator/ContentFolderWorkspaceConfigurator.cs
@@ -0,0 +1,50 @@
+﻿using CMS.ContentEngine;
+using CMS.Membership;
+
+using Kentico.Xperience.Admin.Base.Forms;
+using Kentico.Xperience.Ecommerce.Common.Admin.FormComponentConfigurator;
+
+[assembly: RegisterFormComponentConfigurator(ContentFolderWorkspaceConfigurator.IDENTIFIER, typeof(ContentFolderWorkspaceConfigurator))]
+
+namespace Kentico.Xperience.Ecommerce.Common.Admin.FormComponentConfigurator;
+
+public class ContentFolderWorkspaceConfigurator : FormComponentConfigurator<ContentFolderSelectorComponent>
+{
+    public const string IDENTIFIER = "ContentFolderWorkspaceConfigurator";
+
+    private readonly IContentFolderManager contentFolderManager;
+
+    public ContentFolderWorkspaceConfigurator(IContentFolderManagerFactory contentFolderManagerFactory)
+        => contentFolderManager = contentFolderManagerFactory.Create(UserInfoProvider.AdministratorUser.UserID);
+
+    public override Task Configure(ContentFolderSelectorComponent formComponent, IFormFieldValueProvider formFieldValueProvider, CancellationToken cancellationToken)
+    {
+        if (!formFieldValueProvider.TryGet(nameof(SynchronizationSettingsModel.WorkspaceName), out string? workspaceName) || string.IsNullOrEmpty(workspaceName))
+        {
+            formComponent.AddVisibilityCondition(new Invisible());
+            return Task.CompletedTask;
+        }
+
+        formComponent.Properties.WorkspaceName = workspaceName;
+        return Task.CompletedTask;
+    }
+
+    public override async Task<object> ConfigureValue(IFormFieldValueProvider formFieldValueProvider, string changedFieldName, CancellationToken cancellationToken)
+    {
+        if (string.Equals(changedFieldName, nameof(SynchronizationSettingsModel.WorkspaceName), StringComparison.Ordinal)
+            && formFieldValueProvider.TryGet(nameof(SynchronizationSettingsModel.WorkspaceName), out string? workspaceName)
+            && !string.IsNullOrEmpty(workspaceName))
+        {
+            var newRoot = await contentFolderManager.GetRoot(workspaceName, cancellationToken);
+            return newRoot.ContentFolderID;
+        }
+        return await base.ConfigureValue(formFieldValueProvider, changedFieldName, cancellationToken);
+    }
+}
+
+
+internal class Invisible : VisibilityCondition
+{
+    public override bool Evaluate(IFormFieldValueProvider formFieldValueProvider)
+        => false;
+}
---
+++ src/Kentico.Xperience.Ecommerce.Common/Admin/SynchronizationSettingsModel.cs
@@ -0,0 +1,30 @@
+﻿using CMS.Workspaces.Internal;
+
+using Kentico.Xperience.Admin.Base.FormAnnotations;
+using Kentico.Xperience.Ecommerce.Common.Admin.FormComponentConfigurator;
+using Kentico.Xperience.Ecommerce.Common.Admin.Providers;
+
+namespace Kentico.Xperience.Ecommerce.Common.Admin
+{
+    public class SynchronizationSettingsModel
+    {
+        [RequiredValidationRule]
+        [DropDownComponent(Label = EcommerceCommonConstants.SettingsWorkspaceName, DataProviderType = typeof(WorkspaceOptionsProvider), Order = 0)]
+        public string WorkspaceName { get; set; } = WorkspaceConstants.WORKSPACE_DEFAULT_CODE_NAME;
+
+        [RequiredValidationRule]
+        [ContentFolderSelectorComponent(Label = EcommerceCommonConstants.SettingsProductSKUFolderGuid, Order = 10)]
+        [FormComponentConfiguration(ContentFolderWorkspaceConfigurator.IDENTIFIER, nameof(WorkspaceName))]
+        public int ProductFolderId { get; set; }
+
+        [RequiredValidationRule]
+        [ContentFolderSelectorComponent(Label = EcommerceCommonConstants.SettingsProductVariantFolderGuid, Order = 20)]
+        [FormComponentConfiguration(ContentFolderWorkspaceConfigurator.IDENTIFIER, nameof(WorkspaceName))]
+        public int ProductVariantFolderId { get; set; }
+
+        [RequiredValidationRule]
+        [ContentFolderSelectorComponent(Label = EcommerceCommonConstants.SettingsProductImageFolderGuid, Order = 30)]
+        [FormComponentConfiguration(ContentFolderWorkspaceConfigurator.IDENTIFIER, nameof(WorkspaceName))]
+        public int ImageFolderId { get; set; }
+    }
+}
